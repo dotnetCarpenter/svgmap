ff501232f23cd4defd4eb335af63451ff579f2a4
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-07 01:13:08 +0100

    test filter with typedArray

--- test/filter.js
+++ test/filter.js
@@ -3,10 +3,19 @@
 var test = require("tape");
 var filter = require("../lib/filter");
 
-test('basic filter', function(t) {
+test('array filter', function(t) {
   t.plan(1);
-	var list = [0,1,2,3,4,5,6,7,8,9];
-	var actual = filter(list, n => n > 6);
-	var expected = [7,8,9];
+	let list = [0,1,2,3,4,5,6,7,8,9];
+	let actual = filter(list, n => n > 6);
+	let expected = [7,8,9];
+  t.deepEquals(actual, expected);
+});
+
+test('typedArray filter', function(t) {
+  t.plan(1);
+	//uint8 = {0,1,..,255}
+	let list = Uint8Array.from([0,1,2,3,4,5,6,7,8,9]);
+	let actual = filter(list, n => n > 6);
+	let expected = [7,8,9];
   t.deepEquals(actual, expected);
 });
