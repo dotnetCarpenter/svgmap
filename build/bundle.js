2b83a5428b679b29fcaa4513f234ddc59c4f2587
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-07 01:14:53 +0100

    add filter to bundle

--- build/bundle.js
+++ build/bundle.js
@@ -2,7 +2,9 @@
 
 //let assign = require("../lib/assign");
 var compose = require("../lib/compose");
+var filter = require("../lib/filter");
 
 module.exports = {
-	compose: compose
+	compose: compose,
+	filter: filter
 }
